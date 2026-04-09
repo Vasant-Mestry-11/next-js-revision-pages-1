a5369a6686258205011e6848460a7aa3156ec7ff
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2026-04-09 16:10:39 +0530

    :zap: :art: Login Page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,19 @@
-import styles from "./login.module.css";
+import LoginForm from "@/app/_components/LoginForm";
+import Link from "next/link";
 
 const LoginPage = () => {
-  return <div className={styles.login}>Login Page</div>;
+  return (
+    <div className="max-w-md mx-auto bg-white p-8 rounded-lg shadow-md">
+      <h1 className="text-2xl font-bold mb-6">Login</h1>
+      <LoginForm />
+      <p className="mt-4 text-center">
+        Don`t have an account ?{" "}
+        <Link href="/register" className="text-blue-600 hover:underline">
+          Register
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default LoginPage;
