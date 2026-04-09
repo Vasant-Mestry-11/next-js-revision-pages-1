e32b188f64dff1f8ce86210edc1cd4da546be47f
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2026-04-09 23:39:34 +0530

    :zap: :art: cookie based login

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -3,21 +3,22 @@
 import axios from "axios";
 import { redirect } from "next/navigation";
 import { UserType } from "../_types/auth";
+import { deleteSession, setSession } from "../_lib/session";
 
 const API_URL = "http://localhost:3001/";
 
 export const loginAction = async (formData: FormData) => {
   const email = formData.get("email");
   const password = formData.get("password");
+  console.log("=================", email, password);
   try {
-    const response = await axios.get(
-      `${API_URL}/users?email=${email}&password=${password}`,
-    );
+    const response = await axios.get(`${API_URL}/users?email=${email}`);
     console.log(response.data);
     const user: UserType = await response.data[0];
     if (!user) throw new Error("Invalid Credentials");
 
     // set user in cookie
+    await setSession({ name: user.name, email: user.email, id: user.id });
   } catch (error: unknown) {
     console.log(error);
     throw new Error("Failed to login");
@@ -26,5 +27,6 @@ export const loginAction = async (formData: FormData) => {
 };
 
 export const logoutAction = async () => {
+  await deleteSession();
   redirect("/login");
 };
