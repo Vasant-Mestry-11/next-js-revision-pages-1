46baa02f409a9c98844624f0aae9b02aad4965a3
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2026-04-09 23:49:05 +0530

    :zap: :art: logout functionality implemented

--- src/app/_components/LogoutButton.tsx
+++ src/app/_components/LogoutButton.tsx
@@ -1,5 +1,33 @@
+"use client";
+
+import { redirect, useRouter } from "next/navigation";
+import { logoutAction } from "../actions/auth";
+
 const LogoutButton = () => {
-  return <div>Login Out Button</div>;
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await logoutAction();
+
+      // redirect
+      // client side fallback
+
+      // redirect('/login')
+      router.push("/login");
+      router.refresh()
+    } catch (error) {
+      console.log("Log out failed", error);
+    }
+  };
+  return (
+    <button
+      onClick={handleLogout}
+      className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition-colors cursor-pointer"
+    >
+      Log out
+    </button>
+  );
 };
 
 export default LogoutButton;
